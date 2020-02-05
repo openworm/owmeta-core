fc0b4c5a6ffad0bcca539800d73980d1f6c732f1
Mark Watts <watts.mark2015@gmail.com>
2020-02-04 19:30:26 -0600

    Removing C. elegans references in setup.py
    - Also deleting package_utils cruft

--- setup.py
+++ setup.py
@@ -7,26 +7,14 @@ import sys
 
 
 long_description = """
-owmeta
-======
+owmeta-core
+===========
 
-A unified, simple data access library in Python for data, facts, and models of
-*C. elegans* anatomy for the `OpenWorm project <http://www.openworm.org>`_
-
-What does it do?
-----------------
-
-Enables a simple Python API for asking various questions about the cells of the
-*C. elegans*, enabling the sharing of data about *C. elegans* for the purpose
-of building a `data-to-model pipeline <http://docs.openworm.org/en/latest/projects>`_
-for the OpenWorm project. In addition, it is a repository for various iterations
-of inferred / predicted data about *C. elegans*. Uncontroversial facts and
-inferred information are distinguished through the use of explicit Evidence
-references.
+owmeta-core is a platform for sharing relational data over the internet.
 """
 
 
-for line in open('owmeta/__init__.py'):
+for line in open('owmeta_core/__init__.py'):
     if line.startswith("__version__"):
         version = line.split("=")[1].strip()[1:-1]
 
@@ -41,7 +29,7 @@ def excludes(base):
 
 
 setup(
-    name='owmeta',
+    name='owmeta-core',
     zip_safe=False,
     setup_requires=['pytest-runner'],
     tests_require=[
@@ -91,23 +79,20 @@ setup(
         ]
     },
     version=version,
-    packages=['owmeta',
-              'owmeta.data_trans',
-              'owmeta.commands'],
+    packages=['owmeta_core',
+              'owmeta_core.data_trans',
+              'owmeta_core.commands'],
     author='OpenWorm.org authors and contributors',
     author_email='info@openworm.org',
-    description='A Python library for working with OpenWorm data and models',
+    description='owmeta-core is a platform for sharing relational data over the internet.',
     long_description=long_description,
     license='MIT',
-    url='https://pyopenworm.readthedocs.io/en/latest/',
-    download_url='https://github.com/openworm/owmeta/archive/master.zip',
+    url='https://owmeta-core.readthedocs.io/en/latest/',
+    download_url='https://github.com/openworm/owmeta-core/archive/master.zip',
     entry_points={
-        'console_scripts': ['owm = owmeta.cli:main'],
-        'rdf.plugins.store': [
-            'lazy_pickle = owmeta.lazy_deserialization_store:LazyDeserializationStore'
-        ],
+        'console_scripts': ['owm = owmeta.cli:main']
     },
-    package_data={'owmeta': ['default.conf']},
+    package_data={'owmeta_core': ['default.conf']},
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
