ce1c24b2b504c840efac5adf0c9c51fd86b89195
Mark Watts <watts.mark2015@gmail.com>
2020-02-09 17:12:39 -0600

    Removing defunt test

--- tests/DocumentationTest.py
+++ tests/DocumentationTest.py
@@ -60,31 +60,5 @@ class SphinxTest(unittest.TestCase):
                 optionflags=(ALLOW_UNICODE | doctest.ELLIPSIS), **kwargs)
         self.assertEqual(failure_count, 0)
 
-    def test_adding_data(self):
-        # Setup a class imported by docs for demonstration purposes
-        from owmeta_core.dataobject import DataObject, DatatypeProperty
-        from owmeta_core.context import Context
-        Load = lambda *args, **kwargs: [namedtuple('Record', ('pnum', 'flns', 'hrds'))(12, 1.0, 100)]
-
-        class Widget(DataObject):
-            hardiness = DatatypeProperty()
-            fullness = DatatypeProperty()
-            part_number = DatatypeProperty()
-
-            def identifier_augment(self):
-                return self.make_identifier_direct(str(self.part_number.onedef()))
-
-            def defined_augment(self):
-                return self.part_number.has_defined_value()
-
-        ctx = Context(ident='http://example.org/data/imports/BDW_Widgets_2018-2019')
-        ctx.mapper.process_class(Widget)
-
-        ctx(Widget)(part_number=15)
-        ctx(Widget)(part_number=17)
-        ctx(Widget)(part_number=20)
-
-        self.execute('adding_data', extraglobs={'Load': Load, 'Widget': Widget, 'ctx18': ctx})
-
     def test_making_dataObjects(self):
         self.execute('making_dataObjects')
