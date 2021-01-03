c928a43bf84cdbdea9d74585cd84303cec80cf3c
Mark Watts <watts.mark2015@gmail.com>
2021-01-03 15:31:28 -0600

    Relocating branch check to deploy.sh
    - We can still trigger downstream builds even if we
      are not on the develop branch

--- .travis.success.sh
+++ .travis.success.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -ex
 echo "$TRAVIS_COMMIT_MESSAGE" | head -n1 | grep -q '^MINOR:' && exit 0
 if [ $DEPLOY ] ; then
-    HEAD_REV=$(git rev-parse HEAD)
-    ORIGIN_DEV_REV=$(git ls-remote origin refs/heads/dev | grep -E -o '^[^[:space:]]+')
-    if [ "$HEAD_REV" != "$ORIGIN_DEV_REV" ] ; then
-        echo "Not deploying since we aren't on the 'dev' branch" >&2
-        exit 0
-    fi
     ./check-build-status.sh
     ./deploy.sh
     ./travis-downstream-trigger.sh
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -xe
+HEAD_REV=$(git rev-parse HEAD)
+ORIGIN_DEV_REV=$(git ls-remote origin refs/heads/develop | grep -E -o '^[^[:space:]]+')
+if [ "$HEAD_REV" != "$ORIGIN_DEV_REV" ] ; then
+    echo "Not deploying since we aren't on the 'develop' branch" >&2
+    exit 0
+fi
 
 date=$(date +"%Y%m%d%H%M%S")
 sed -i -r "s/__version__ = '([^']+)\\.dev0'/__version__ = '\\1.dev$date'/" owmeta_core/__init__.py
