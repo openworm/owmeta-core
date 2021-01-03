24b0e0d1658f5f21294c8da824e43e6ece6cbdd8
Mark Watts <watts.mark2015@gmail.com>
2021-01-03 16:40:44 -0600

    Adding bdist_wheel to dev deploy

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ fi
 
 date=$(date +"%Y%m%d%H%M%S")
 sed -i -r "s/__version__ = '([^']+)\\.dev0'/__version__ = '\\1.dev$date'/" owmeta_core/__init__.py
-python setup.py egg_info sdist
+python setup.py egg_info sdist bdist_wheel
 twine upload -c "Built by travis-ci. Uploaded after $(date +"%Y-%m-%d %H:%M:%S")" dist/owmeta-core*tar.gz dist/owmeta_core*tar.gz
